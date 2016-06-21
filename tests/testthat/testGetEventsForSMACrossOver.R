696006b18c1a8d15c99e6565bdecd72fe3c56c44
justin nafe <justinjnafe@gmail.com>
2016-06-21 08:09:27 -0500

    Write unit tests

--- tests/testthat/testGetEventsForSMACrossOver.R
+++ tests/testthat/testGetEventsForSMACrossOver.R
@@ -2,7 +2,22 @@ library(TechnicalEvents)
 context("GetEventsForSMACrossOver")
 
 data("MSFT")
+test.data.dates <- seq(as.Date("2015/12/1"), as.Date("2016/2/1"), "days")
+test.data.values <- sin(seq(1, length(test.data.dates)))
+test.xts <- as.xts(test.data.values, order.by = test.data.dates)
 
 test_that("GetEventsForSMACrossOver", {
-  expect_equal(nrow(GetEventsForSMACrossOver(Ad(MSFT))), nrow(MSFT))
+  expect_equal(dim(GetEventsForSMACrossOver(Ad(MSFT))), c(nrow(MSFT), 1))
+
+  expect_that(GetEventsForSMACrossOver(MSFT), throws_error("1 is not TRUE"))
+
+  # Error when the fast is >= the slow
+  expect_that(GetEventsForSMACrossOver(Ad(MSFT), 50, 50), throws_error("slowSma > fastSma is not TRUE"))
+
+  # Return NULL when the slowSma is >= the number of rows.
+  expect_named(GetEventsForSMACrossOver(test.xts, 20, nrow(test.xts)), NULL)
+
+  # The number of NAs should match the slowSma
+  expect_length(GetEventsForSMACrossOver(test.xts, 10, 20)[is.na(GetEventsForSMACrossOver(test.xts, 10, 20)),], 20)
+
 })
